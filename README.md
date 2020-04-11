fe05fd35521300fbc3f633ea9ce85fe93baca6ba
v.chesnokov <ov7a@yandex.ru>
2020-04-12 01:03:28 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# telegram_publisher
+This is a simple script that parses a Jekyll post written in markdown and sends it to a telegram channel.
+
+Is was quite frustrating to copy these posts manually. Reasons:
+1. Telegram Desktop [does not support](https://github.com/telegramdesktop/tdesktop/issues/4737) markdown links formatting: `[text](http://example.com)`. You should manually paste links [using hotkeys](https://github.com/telegramdesktop/tdesktop/issues/4336). That is really annoyng.
+2. Telegram Desktop [does not support](https://github.com/telegramdesktop/tdesktop/issues/5795) rich format text copy & paste. It can do only plain text. So you can't copy a pretty formatted text from a web page or 
+3. The layout of a post slightly differs between the blog and the channel.
+4. Post scheduling using the script is quicker.
+
---
+++ publisher.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import os
+import sys
+from telethon.sync import TelegramClient
+import socks
+import frontmatter
+from datetime import datetime
+
+content_path = sys.argv[1]
+
+UTC_HOUR = 7
+date_parts = list(map(int, os.path.basename(content_path).split('-')[0:3]))
+post_date = datetime(year=date_parts[0], month=date_parts[1], day=date_parts[2], hour=UTC_HOUR, second=1)
+
+post = frontmatter.load(content_path)
+updated_content = "\n\n".join([
+	", ".join(map(lambda tag: "#" + tag, post['tags'])), 
+	post['title'],
+	post.content
+])
+
+def get_env(name, message, cast=str):
+	if name in os.environ:
+		return os.environ[name]
+	while True:
+		value = input(message)
+		try:
+			return cast(value)
+		except ValueError as e:
+			print(e, file=sys.stderr)
+			time.sleep(1)
+
+session = os.environ.get('TG_SESSION', 'publisher')
+api_id = get_env('TG_API_ID', 'Enter your API ID: ', int)
+api_hash = get_env('TG_API_HASH', 'Enter your API hash: ')
+
+channel_name='minutkaprosvescheniya' 
+
+proxy = (socks.SOCKS5, 'localhost', 9050) 
+	
+with TelegramClient(session, api_id, api_hash, proxy=proxy) as client:
+	client.send_message(channel_name, updated_content, parse_mode='md', schedule=post_date)
+
+
+	
